fd7fa3ec16cb78f68115788cb656bc5942084b73
myr-tech <mayur.surbhalavi@gmail.com>
2023-06-06 06:34:59 +0530

    Create fullname.js

---
+++ fullname.js
@@ -0,0 +1,37 @@
+/*Author:mayur satisfies. surbhalavi
+created date:3.6.2023
+Description:fullname
+company name:arc
+project name: none
+
+
+*/
+
+
+function findsqof(num)
+{
+    var sqr=num*num;
+    
+    return sqr;
+}
+
+// var sqr=findsqof(4);
+// console.log('square of number is',sqr);
+
+
+
+
+// multiresult
+/*function multiplier(num)
+{
+    var sqr=num*num;
+    var cube=num*num*num;
+    
+    return {'squre':sqr,'cube':cube};
+}
+
+var obj=multiplier(2);
+console.log(obj.squre);
+console.log(obj.cube);*/
+
+
